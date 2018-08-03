2c65c9370ea292583e8ea36e37ffbd034ba2dd21
Meetacoo <2283785225@qq.com>
2018-08-03 18:19:16 +0800

    add express

--- 18.7.30-MongoDB/11-mongoose-wish/Model/Wish.js
+++ 18.7.30-MongoDB/11-mongoose-wish/Model/Wish.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/wishwall',{ useNewUrlParser: true });
-function getRandom(min,max){
-	return Math.round(min + (max - min) * Math.random());
-}
+
 // console.log(mongoose);
 var db = mongoose.connection;
 
---
+++ 18.8.03-Express/02-route/01-express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+// console.log(express);
+let app = express();
+// console.log(app);
+app.get('/', function(req, res){
+	res.send('get data ...');
+});
+// get('/路径')
+app.get('/abc', function(req, res){
+	res.send('get abc data ...');
+});
+app.post('/', function(req, res){
+	res.send('post data ...');
+});
+app.put('/', function(req, res){
+	res.send('put data ...');
+});
+app.delete('/', function(req, res){
+	res.send('delete data ...');
+});
+
+app.use(express.static('public'));
+
+app.listen(3000,'127.0.0.1',()=>{
+	console.log('running on server 127.0.0.1:3000');
+})
